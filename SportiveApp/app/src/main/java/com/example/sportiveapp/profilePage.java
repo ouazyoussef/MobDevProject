61183e0b75340641f057476c8026faa49bd9b1bc
ToufiqMarwan <marwan.toufiq@efrei.net>
2022-06-16 13:29:21 +0200

    Reglage profile page

--- SportiveApp/app/src/main/java/com/example/sportiveapp/profilePage.java
+++ SportiveApp/app/src/main/java/com/example/sportiveapp/profilePage.java
@@ -1,5 +1,6 @@
 package com.example.sportiveapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,30 @@ public class profilePage extends AppCompatActivity {
         History = findViewById(R.id.history);
         CreateMatch = findViewById(R.id.createMatch);
         ProximityStade = findViewById(R.id.proximityStade);
+        Stats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(profilePage.this, StatsActivity.class));
+            }
+        });
+        History.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(profilePage.this, History.class));
+            }
+        });
+        CreateMatch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(profilePage.this, CreateMatch.class));
+            }
+        });
+        ProximityStade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(profilePage.this, ProximityStade.class));
+            }
+        });
 
     }
 }
